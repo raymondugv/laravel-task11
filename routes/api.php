7e3e8bc1de4ac93c12c6a83c13c602b0fbac783e
KamPC <github@lekienhoanh.dev>
2023-06-17 12:47:51 +0700

    added Request, Resource and API route for Website

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route for Website resource
+Route::apiResource('websites', App\Http\Controllers\Api\V1\WebsiteController::class);
