97bc4abcd5126208ce20ae05a370c93faff64904
KamPC <github@lekienhoanh.dev>
2023-06-17 14:12:30 +0700

    update email

---
+++ app/Mail/NewPostNotification.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Mail;
+
+use App\Models\Post;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class NewPostNotification extends Mailable
+{
+    use Queueable;
+    use SerializesModels;
+    protected $post;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'New Post Notification',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            html: "<h1>New Post Notification</h1><p>Post Title: $this->post->title</p><p>Post Content: $this->post->content</p>",
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
