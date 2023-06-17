fc273d2fded966e159efcf2a31beadfc838d7c30
KamPC <github@lekienhoanh.dev>
2023-06-17 13:30:45 +0700

    update relatioship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUuids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -16,4 +17,9 @@ class Post extends Model
         'body',
         'website_id',
     ];
+
+    public function website(): BelongsTo
+    {
+        return $this->belongsTo(Website::class);
+    }
 }
