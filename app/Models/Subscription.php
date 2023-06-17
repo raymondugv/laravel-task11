83bcaa03af51dcae4dcf171ab607d2df6b46c528
KamPC <github@lekienhoanh.dev>
2023-06-17 14:12:15 +0700

    add subscription table

---
+++ app/Models/Subscription.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Subscription extends Model
+{
+    use HasFactory;
+    use HasUuids;
+
+    protected $fillable = [
+        'user_id',
+        'website_id',
+    ];
+
+    public function website(): BelongsTo
+    {
+        return $this->belongsTo(Website::class);
+    }
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
